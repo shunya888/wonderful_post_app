59e82a5f875ca772b8bbcfb6eaced12b1cd30301
金子瞬也 <>
2023-09-07 18:09:25 +0900

    トップページのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :sample_articles
+  root to: 'articles#index'
   resources :articles
+  resources :sample_articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
