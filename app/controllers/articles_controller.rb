412db2e29caf43ad11ac0400973c5c590b48e193
金子瞬也 <>
2023-09-09 00:05:25 +0900

    articleの編集が出来るように edit, updateメソッドの実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  # GET /articles/1/edit
+  def edit
+  end
+
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
@@ -25,6 +29,19 @@ class ArticlesController < ApplicationController
         format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /articles/1 or /articles/1.json
+  def update
+    respond_to do |format|
+      if @article.update(article_params)
+        format.html { redirect_to article_url(@article), notice: "article was successfully updated." }
+        format.json { render :show, status: :ok, location: @article }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
