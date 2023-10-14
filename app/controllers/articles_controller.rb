954d4d6030eaefd21313755e6e768d5bb9140fc5
金子瞬也 <>
2023-10-14 22:53:19 +0900

    記事の作成, 編集, 削除をログインユーザーに紐づけて処理させるよう実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
+  skip_before_action :authenticate_user!, only: %i[ index show ]
+  before_action :set_article, only: %i[ edit update destroy ]
 
   # GET /articles or /articles.json
   def index
@@ -8,6 +9,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1 or /articles/1.json
   def show
+    @article = Article.find(params[:id])
   end
 
   # GET /articles/new
@@ -21,52 +23,33 @@ class ArticlesController < ApplicationController
 
   # POST /articles or /articles.json
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.new(article_params)
     if @article.save
-      redirect_to @article, notice: "#{t('activerecord.models.article')}が作成されました。"
+      redirect_to @article, notice: "#{t('activerecord.models.article')}を作成しました。"
     else
       render :new, status: :unprocessable_entity
     end
-    # respond_to do |format|
-    #   if @article.save
-    #     format.html { redirect_to article_url(@article), notice: "article was successfully created." }
-    #     format.json { render :show, status: :created, location: @article }
-    #   else
-    #     format.html { render :new, status: :unprocessable_entity }
-    #     format.json { render json: @article.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
-    # respond_to do |format|
       if @article.update(article_params)
         redirect_to @article, notice: "#{t('activerecord.models.article')}を編集しました。"
-        # format.html { redirect_to article_url(@article), notice: "article was successfully updated." }
-        # format.json { render :show, status: :ok, location: @article }
       else
-        render :new, status: :unprocessable_entity
-        # format.html { render :edit, status: :unprocessable_entity }
-        # format.json { render json: @article.errors, status: :unprocessable_entity }
+        render :edit, status: :unprocessable_entity
       end
-    # end
   end
 
   # DELETE /articles/1 or /articles/1.json
   def destroy
     @article.destroy
     redirect_to articles_url, notice: "#{t('activerecord.models.article')}を削除しました。"
-    # respond_to do |format|
-    #   format.html { redirect_to articles_url, notice: "article was successfully destroyed." }
-    #   format.json { head :no_content }
-    # end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
-      @article = Article.find(params[:id])
+      @article = current_user.articles.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
