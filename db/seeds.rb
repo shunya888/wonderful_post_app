58fe874d4a3121cf14d4d4422f5b2ff3a27b1bcb
金子瞬也 <>
2023-11-03 20:55:43 +0900

    seed ファイルに初期データの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |n|
+  n += 1
+  user = User.find_or_create_by!(email: "user00#{n}@test.com") do |u|
+    u.password = "test1234"
+  end
+
+  50.times do |i|
+    i += 1
+    user.articles.find_or_create_by!(title: "No.#{i}: user00#{n}の記事") do |article|
+      article.content = "No.#{i}: user00#{n}の記事の本文"
+    end
+  end
+end
