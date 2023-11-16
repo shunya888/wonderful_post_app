d322a5b964d72a4c915cf09a29defd5c429624a2
金子瞬也 <>
2023-11-16 10:13:00 +0900

    検索フォームの作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,9 @@ class ArticlesController < ApplicationController
 
   # GET /articles or /articles.json
   def index
-    @articles = Article.all.page params[:page]
+    articles = Article.all
+    articles = articles.where("title LIKE ?", "%#{params[:title]}%") if params[:title].present?
+    @articles = articles.page params[:page]
   end
 
   # GET /articles/1 or /articles/1.json
